2cd99397bfc954c3b831c6443c8153f1b80e619e
lidecai <2073399839@qq.com>
2019-07-04 20:15:58 +0800

    session

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 const express = require('express');
 // cookieParser用来读取cookie的
 const cookieParser = require('cookie-parser');
-
+// session
+const cookieSession = require('cookie-session')
 let server = express();
 
 
 // cookie
-server.use(cookieParser('987987987qweqweqwe'));
 
+server.use(cookieParser());
+server.use(cookieSession({
+    name:'lidecai',
+    keys: ['aaa', 'bbb', 'ccc'],
+    maxAge:20*1000
+}));
 server.use('/', function (req, res) {
-   res.clearCookie('user');
-   
+    if(req.session['count']==null){
+        req.session['count']=1;
+    }else{
+       req.session['count']++;
+    }
+    console.log(req.session['count'])
     res.send('11112222');
 });
 
