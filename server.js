73fe748a9a8b6535f47d7795cd44f8ac5300ffdd
lidecai <2073399839@qq.com>
2019-07-04 22:04:50 +0800

    来与则注释

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-//引入express框架
-const express = require('express');
-// cookieParser用来读取cookie的
-const cookieParser = require('cookie-parser');
-// session
-const cookieSession = require('cookie-session')
-let server = express();
+// //引入express框架
+// const express = require('express');
+// // cookieParser用来读取cookie的
+// const cookieParser = require('cookie-parser');
+// // session
+// const cookieSession = require('cookie-session')
+// let server = express();
 
 
-// cookie
+// // cookie
 
-server.use(cookieParser());
-server.use(cookieSession({
-    name:'lidecai',
-    keys: ['aaa', 'bbb', 'ccc'],
-    maxAge:20*1000
-}));
-server.use('/', function (req, res) {
-    if(req.session['count']==null){
-        req.session['count']=1;
-    }else{
-       req.session['count']++;
-    }
-    console.log(req.session['count'])
-    res.send('11112222');
-});
+// server.use(cookieParser());
+// server.use(cookieSession({
+//     name:'lidecai',
+//     keys: ['aaa', 'bbb', 'ccc'],
+//     maxAge:20*1000
+// }));
+// server.use('/', function (req, res) {
+//     if(req.session['count']==null){
+//         req.session['count']=1;
+//     }else{
+//        req.session['count']++;
+//     }
+//     console.log(req.session['count'])
+//     res.send('11112222');
+// });
 
-// 端口
-server.listen(1996);
+// // 端口
+// server.listen(1996);
